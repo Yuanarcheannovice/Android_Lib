da3f33e59cfe4247904ea510cb896c470a2ca462
YuanArcheanNovice <624117230@qq.com>
2019-01-17 10:55:05 +0800

    update fresco util

--- lib_util_fresco/src/main/java/com/archeanx/lib/util/fresco/FrescoUtil.java
+++ lib_util_fresco/src/main/java/com/archeanx/lib/util/fresco/FrescoUtil.java
@@ -1,12 +1,20 @@
 package com.archeanx.lib.util.fresco;
 
+import android.graphics.drawable.Animatable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.view.ViewGroup;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeController;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.postprocessors.IterativeBoxBlurPostProcessor;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
@@ -99,6 +107,66 @@ public class FrescoUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    /**
+     * 显示缩略图
+     *
+     * @param draweeView     draweeView
+     * @param url            url
+     * @param resizeWidthPx  resizeWidth
+     * @param resizeHeightPx resizeHeight
+     */
+    public static void showThumb(SimpleDraweeView draweeView, String url, int resizeWidthPx, int resizeHeightPx) {
+        if (url == null || "".equals(url))
+            return;
+        if (draweeView == null)
+            return;
+        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url))
+                .setResizeOptions(new ResizeOptions(resizeWidthPx, resizeHeightPx))
+                .build();
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setImageRequest(request)
+                .setOldController(draweeView.getController())
+                .setControllerListener(new BaseControllerListener<ImageInfo>())
+                .build();
+        draweeView.setController(controller);
+    }
+
+    /**
+     * 通过imageWidth 的宽度，自动适应高度
+     * * @param simpleDraweeView view
+     * * @param imagePath  Uri
+     * * @param imageWidth width
+     */
+    public static void setControllerListener(final SimpleDraweeView simpleDraweeView, String imagePath, final int imageWidth) {
+        final ViewGroup.LayoutParams layoutParams = simpleDraweeView.getLayoutParams();
+        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+            @Override
+            public void onFinalImageSet(String id, @Nullable ImageInfo imageInfo, @Nullable Animatable anim) {
+                if (imageInfo == null) {
+                    return;
+                }
+                int height = imageInfo.getHeight();
+                int width = imageInfo.getWidth();
+                layoutParams.width = imageWidth;
+                layoutParams.height = (int) ((float) (imageWidth * height) / (float) width);
+                simpleDraweeView.setLayoutParams(layoutParams);
+            }
+
+            @Override
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+                //Log.d("TAG", "Intermediate image received");
+            }
+
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        };
+        DraweeController controller = Fresco.newDraweeControllerBuilder().setControllerListener(controllerListener).setUri(Uri.parse(imagePath)).build();
+        simpleDraweeView.setController(controller);
     }
 
 }
