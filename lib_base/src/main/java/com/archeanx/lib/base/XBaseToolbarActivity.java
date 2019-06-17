ccbdf31b05435171738ddcd97e867146490a3b48
YuanArcheanNovice <624117230@qq.com>
2019-06-17 11:06:01 +0800

    完善 toolbar

--- lib_base/src/main/java/com/archeanx/lib/base/XBaseToolbarActivity.java
+++ lib_base/src/main/java/com/archeanx/lib/base/XBaseToolbarActivity.java
@@ -1,6 +1,8 @@
 package com.archeanx.lib.base;
 
+import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,10 +32,22 @@ public abstract class XBaseToolbarActivity extends XBaseActivity implements View
         onInitViewId();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
-     * 初始化view
+     * @param savedInstanceState savedInstanceState
+     * @return 设置layout
      */
-    protected abstract void onInitViewId();
+    @Override
+    @LayoutRes
+    protected abstract int onSetContentView(Bundle savedInstanceState);
 
     /**
      * 必须 指定toolbar id
@@ -44,14 +58,40 @@ public abstract class XBaseToolbarActivity extends XBaseActivity implements View
 
     protected abstract int showToolbar();
 
+    /**
+     * 初始化view
+     */
+    protected abstract void onInitViewId();
 
+
+    /**
+     * 设置监听
+     */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    protected abstract void onSetListener();
+
+
+    /**
+     * 点击事件
+     *
+     * @param v 被点击的view
+     */
+    @Override
+    public abstract void onClick(View v);
+
+    /**
+     * 开始数据
+     */
+    @Override
+    protected abstract void onInitData();
+
+    /**
+     * 方法说明:手动释放内存
+     * 方法名称:releaseMemory
+     * 返回void
+     */
+    @Override
+    protected abstract void onCloseActivity();
+
 
 }
