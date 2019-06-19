997f753fd70c0ac14ca7bdebac86a42c85d8a23d
1339390910@qq.com <1339390910@qq.com>
2019-06-20 00:24:42 +0800

    修改adapter adddatas 溢出问题

--- lib_adapter/src/main/java/com/archeanx/lib/adapter/XRvPureDataAdapter.java
+++ lib_adapter/src/main/java/com/archeanx/lib/adapter/XRvPureDataAdapter.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 /**
  * @author xz
- *  2017/7/27 0027
+ * 2017/7/27 0027
  * 增加了纯净Adapter，的数据操作
  * <p>
  * 因为有些时候可是使用的map集合，所以改用继承来写数据操作，不合并到 RvPureAdapte中
@@ -98,7 +98,7 @@ public abstract class XRvPureDataAdapter<T> extends XRvPureAdapter {
         } else {
             this.mDatas.addAll(data);
             if (isRefresh) {
-                notifyItemRangeInserted(mDatas.size() - 1, data.size());
+                notifyItemRangeInserted(mDatas.size() - data.size() - 1, data.size());
             }
         }
     }
