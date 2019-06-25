2172fec49d2e33af5370075bef9343af8496d3be
YuanArcheanNovice <624117230@qq.com>
2019-06-25 18:18:49 +0800

    修复app_update

--- lib_app_update/src/main/java/com/archeanx/lib/appupdate/AppUpdateManager.java
+++ lib_app_update/src/main/java/com/archeanx/lib/appupdate/AppUpdateManager.java
@@ -95,11 +95,6 @@ public class AppUpdateManager {
      * @param isForce           是否强制更新  true-强制
      */
     public void inspectVersion(String notificationTitle, final String apkUrl, String tipMessage, boolean isForce) {
-        // 检查8.0 安装权限
-        if (!AppUpdateUtil.inspectInstallApk(mContext)) {
-            return;
-        }
-
         mDownloadTitle = notificationTitle;
         if (TextUtils.isEmpty(apkUrl)) {
             throw new NullPointerException("appUrl is null");
